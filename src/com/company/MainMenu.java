cea7bb08648f6e4201a910a9e24e9fe2cbf0a855
Chatchat Paisalpanich <frinttropystudy222@gmail.com>
2021-06-26 14:14:19 +0700

    Create empty SortPage

--- src/com/company/MainMenu.java
+++ src/com/company/MainMenu.java
@@ -124,8 +124,6 @@ public class MainMenu {
         frame.add(beginSortButton);
 
 
-
-
         // Making it all visible
         frame.setVisible(true);
 
---
+++ src/com/company/SortPage.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import javax.swing.*;
+
+public class SortPage {
+
+    private JFrame frame;
+    private JLabel prompt;
+
+    private ImageIcon leftImage;
+    private ImageIcon rightImage;
+
+}
